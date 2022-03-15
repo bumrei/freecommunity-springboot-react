403b7b25c89f857a0f42124d646d3f2cb5ced192
bumrei <bumrei@gmail.com>
2022-03-15 16:43:00 +0900

    초반 세팅

---
+++ src/main/java/org/commu/freecommunity/FreeCommunityApplication.java
@@ -0,0 +1,13 @@
+package org.commu.freecommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/commu/freecommunity/FreeCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.commu.freecommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
